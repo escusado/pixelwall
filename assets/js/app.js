e7f5d42c783cfe7530bb6304dffdaada64f7b039
Joaquin <escusado@gmail.com>
2015-02-08 17:08:16 -0600

    animated-random-canvas

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,26 +29,31 @@ Class('App').inherits(Widget)({
       var context = this.ctx;
 
       // Creates an Image Object of size 500*200
-      var imageData = context.createImageData(canvasSize.w, canvasSize.h);
+      this.imageData = context.createImageData(canvasSize.w, canvasSize.h);
 
       // Takes data in the Canvas Pixel Array
-      var CanvasPixelArray = imageData.data;
+      this.CanvasPixelArray = this.imageData.data;
 
       // Calculate total no of pixels
-      var total_pixel = imageData.width*imageData.height;
+      this.total_pixel = this.imageData.width*this.imageData.height;
 
+      window.requestAnimationFrame(this.update.bind(this));
+
+      return;
+    },
+
+    update : function update(){
       // Random RGBa values for each pixel
-      for (var i = 0; i < total_pixel; i++) {
-        CanvasPixelArray[i*4] = Math.floor(Math.random()*255);
-        CanvasPixelArray[i*4+1] = Math.floor(Math.random()*255);
-        CanvasPixelArray[i*4+2] = Math.floor(Math.random()*255);
-        CanvasPixelArray[i*4+3] = 255;
+      for (var i = 0; i < this.total_pixel; i++) {
+        this.CanvasPixelArray[i*4] = Math.floor(Math.random()*255);
+        this.CanvasPixelArray[i*4+1] = Math.floor(Math.random()*255);
+        this.CanvasPixelArray[i*4+2] = Math.floor(Math.random()*255);
+        this.CanvasPixelArray[i*4+3] = 255;
       };
 
       // Display the Image
-       context.putImageData(imageData, 0, 0);
-
-      return;
+       this.ctx.putImageData(this.imageData, 0, 0);
+       window.requestAnimationFrame(this.update.bind(this));
     }
   }
 });
